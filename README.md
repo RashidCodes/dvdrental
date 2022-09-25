ece20fa2c66a1c083f2d0d0574d0a241cea79bf7
Kingmoh <rashidmincan@gmail.com>
2022-09-25 22:02:20 +1000

    Push to dockerhub

--- README.md
+++ README.md
@@ -2,4 +2,18 @@
 
 Containerised ELT Pipeline for a DVD Rental Store
 
+```bash 
+
+./runs.sh
+
+```
+
+<br/>
+
+# Image 
+
+```bash
+docker pull kingmoh/dvdrental
+```
+
 
--- runs.sh
+++ runs.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# build the image
 docker build . -t kingmoh/dvdrental:1.1   
+
+# run the container
 docker run --rm --env-file .env --name dvdrental kingmoh/dvdrental:1.1       
